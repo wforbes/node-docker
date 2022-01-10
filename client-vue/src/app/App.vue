1df4b940e1d7c760c95ff7f498fb365f493840d0
Will Forbes <wforbes87@gmail.com>
2022-01-10 00:13:50 -0800

    Vue - move app bar into its own component

--- client-vue/src/app/App.vue
+++ client-vue/src/app/App.vue
@@ -1,20 +1,6 @@
 <template>
 	<v-app>
-		<v-app-bar app color="primary" dark>
-			<div class="d-flex align-center">
-				<router-link class="text-h4 white--text norm-link" :to="'/'">
-					node-docker
-				</router-link>
-			</div>
-
-			<v-spacer></v-spacer>
-
-			<v-btn href="https://wforbes.net/" target="_blank" text>
-				<span class="mr-2">Author's Site</span>
-				<v-icon>mdi-open-in-new</v-icon>
-			</v-btn>
-		</v-app-bar>
-
+		<app-bar />
 		<v-main>
 			<router-view />
 		</v-main>
@@ -22,9 +8,12 @@
 </template>
 
 <script>
+import AppBar from "@/app/components/AppBar.vue";
 export default {
 	name: "App",
-
+	components: {
+		AppBar
+	},
 	data: () => ({
 		//
 	})
---
+++ client-vue/src/app/components/AppBar.vue
@@ -0,0 +1,25 @@
+<template>
+	<v-app-bar app color="primary" dark>
+		<div class="d-flex align-center">
+			<router-link class="text-h4 white--text norm-link" :to="'/'">
+				node-docker
+			</router-link>
+		</div>
+
+		<v-spacer></v-spacer>
+
+		<v-btn href="https://wforbes.net/" target="_blank" text>
+			<span class="mr-2">Author's Site</span>
+			<v-icon>mdi-open-in-new</v-icon>
+		</v-btn>
+	</v-app-bar>
+</template>
+
+<script>
+export default {
+	name: "AppBar",
+	data() {
+		return {};
+	}
+};
+</script>
